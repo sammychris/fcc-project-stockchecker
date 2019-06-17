5f9dd4f87d5a00e60896a21271b287177cbe0dd5
Samuel Christopher <Ebusameric@gmail.com>
2019-06-17 20:08:28 +0100

    test: complete all test cases for endpoint

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -22,27 +22,94 @@ suite('Functional Tests', function() {
         .get('/api/stock-prices')
         .query({stock: 'goog'})
         .end(function(err, res){
-          
-          //complete this one too
-          
+          assert.equal(res.status, 200);
+          assert.property(res.body, 'stockData');
+          assert.property(res.body.stockData, 'stock');
+          assert.property(res.body.stockData, 'price');
+          assert.equal(res.body.stockData.stock, 'GOOG');
+          assert.isNumber(res.body.stockData.price);
           done();
         });
       });
       
       test('1 stock with like', function(done) {
-        
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: 'goog', like: 'true'})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.property(res.body, 'stockData');
+          assert.property(res.body.stockData, 'stock');
+          assert.property(res.body.stockData, 'price');
+          assert.property(res.body.stockData, 'like');
+          assert.equal(res.body.stockData.stock, 'GOOG');
+          assert.isNumber(res.body.stockData.price);
+          assert.equal(res.body.stockData.like, 1);
+          done();
+        });
       });
       
       test('1 stock with like again (ensure likes arent double counted)', function(done) {
-        
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({stock: 'goog', like: 'true'})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.property(res.body, 'stockData');
+          assert.property(res.body.stockData, 'stock');
+          assert.property(res.body.stockData, 'price');
+          assert.property(res.body.stockData, 'like');
+          assert.equal(res.body.stockData.stock, 'GOOG');
+          assert.isNumber(res.body.stockData.price);
+          assert.equal(res.body.stockData.like, 1);
+          done();
+        });
       });
       
       test('2 stocks', function(done) {
-        
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({ stock: [ 'goog', 'msft' ] })
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.property(res.body, 'stockData');
+
+          assert.property(res.body.stockData[0], 'stock');
+          assert.property(res.body.stockData[0], 'price');
+          assert.equal(res.body.stockData[0].stock, 'GOOG');
+          assert.isNumber(res.body.stockData[0].price);
+
+          assert.property(res.body.stockData[1], 'stock');
+          assert.property(res.body.stockData[1], 'price');
+          assert.equal(res.body.stockData[1].stock, 'MSFT');
+          assert.isNumber(res.body.stockData[1].price);
+          done();
+        });
       });
       
       test('2 stocks with like', function(done) {
-        
+        chai.request(server)
+        .get('/api/stock-prices')
+        .query({ stock: [ 'goog', 'msft' ], like: 'true' })
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.property(res.body, 'stockData');
+
+          assert.property(res.body.stockData[0], 'stock');
+          assert.property(res.body.stockData[0], 'price');
+          assert.property(res.body.stockData[0], 'rel_like');
+          assert.equal(res.body.stockData[0].stock, 'GOOG');
+          assert.isNumber(res.body.stockData[0].price);
+          assert.equal(res.body.stockData[0].rel_like, -1);
+
+          assert.property(res.body.stockData[1], 'stock');
+          assert.property(res.body.stockData[1], 'price');
+          assert.property(res.body.stockData[1], 'rel_like');
+          assert.equal(res.body.stockData[1].stock, 'MSFT');
+          assert.isNumber(res.body.stockData[1].price);
+          assert.equal(res.body.stockData[1].rel_like, 1);
+          done();
+        });
       });
       
     });
